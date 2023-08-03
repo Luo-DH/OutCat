0d4c57e690661820833b6538ba61c54723d99690
vansingluo <vansingluo@tencent.com>
2023-08-03 19:33:19 +0800

    --feat 添加数据中心

---
+++ app/src/main/java/com/cheat/outcat/OutCatDataCenter.kt
@@ -0,0 +1,15 @@
+package com.cheat.outcat
+
+import java.util.LinkedList
+
+object OutCatDataCenter {
+
+    val mSelectedDateList: LinkedList<String> = LinkedList()
+
+    val mAllDateList: LinkedList<String> = LinkedList()
+
+    val mSelectedPriceList: LinkedList<Int> = LinkedList()
+
+    val mAllPriceList: LinkedList<Int> = LinkedList()
+
+}
